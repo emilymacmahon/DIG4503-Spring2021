50782c22c78fbdd2675bab953a512e59af3156a6
emilymacmahon <70771497+emilymacmahon@users.noreply.github.com>
2021-02-13 19:14:05 -0500

    Update App.js
    this is full of errors, have to come back

--- lab4/src/App.js
+++ lab4/src/App.js
@@ -14,11 +14,21 @@ Use an internal property of its state (object) called items (array)
 Import MarketItem
 Allow a user to click a button (using the onClick event). It should:
 Add a new MarketItem component to the state.items.
-When adding a new MarketItem component, pass it the current number of entries in state.items as attributes named count and key
-Show a listing of all current entries in state.items using the map() (Links to an external site.) method of Array (Links to an external site.), returning the current element in the Array.
+
+When adding a new MarketItem component, pass it the current number of 
+entries in state.items as attributes named count and key
+
+Show a listing of all current entries in state.items using the map() 
+(Links to an external site.) method of Array (Links to an external site.), 
+returning the current element in the Array.
+
 MarketItem (a function component) should:
 Accept props
-Render a <div> containing a single <p> with the word "Item" followed by the props.count value such that each item will render "Item X" where X is its current number.
+
+Render a <div> containing a single <p> with the word "Item" followed 
+by the props.count value such that each item will render "Item X" where 
+X is its current number.
+
 Not contain or use state of any kind
 App (either a function or class component) should:
 Not contain its default content
@@ -34,28 +44,39 @@ Return or render() only the Market component
 
 
 
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends ReactComponent {
+
+constructor(props){
+  super(props);
+  this.state -
+  items[]
+};
+
+render(){
+  return(
+    <div> 
+      <h2>click to add item </h2> 
+      <button onCLick={()=>{
+        const MarketItem = this.state.items;
+        items.push(<p> Item X </p>);
+        this.setState({items + items});
+
+
+      }}>Click Me </button>
+    </div> {
+      this.state.items.map(function(item, index){
+        return <p key=[index]> [item]</p>
+      })
+    }</div>
 }
+}
+
+
+
+
+
+
 
 export default App;
