f11045fbab19ff4a96dd4b0624b795989a2b96eb
emilymacmahon <70771497+emilymacmahon@users.noreply.github.com>
2021-02-20 18:52:01 -0500

    Update App.js
    figuring it out

--- lab5/src/App.js
+++ lab5/src/App.js
@@ -1,33 +1,40 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import "./App.css";
 import axios from 'axios';
 
 const App = () => {
+  //so I can see in the console the api is working
   const [pokemon, setPokemon] = useState("pikachu");
   //empty array
   const [pokemonData, setPokemonData] = useState([]);
   //empty string
-  const [pokemonType, setPokemonType] = useState("")
+  const [pokemonType, setPokemonType] = useState("");
+
+  const handleChange = (e) => {
+    setPokemon(e.target.value.toLowerCase());
+  };
+
+  const handleSubmit= (e) => {
+    e.preventDefault();
+    getPokemon();
+  };
 
   const getPokemon = async () => {
     const toArray = [];
-    try{
+    try {
       //fetching the url
       //${pokemon} gathers an individual pokemon that is searched for
-      const url ='https://pokeapi.co/api/v2/pokemon/${pokemon}';
+      const url =`https://pokeapi.co/api/v2/pokemon/${pokemon}`;
       const res = await axios.get(url);
       toArray.push(res.data);
       setPokemonType(res.data.types[0].type.name);
       //setting the actual pokemon data
-      setPokemonData(toArray);
-      console.log(res);
-      
-      
-    }  //catches any errors and logs them in console
-    catch (e) {
+      setPokemonData(toArray); //catches any errors and logs them in console
+        }  catch (e) {
       console.log(e)
     }
   };
+  console.log(pokemonData);
 
  //prevent default so it doesn't reload the page
  //to lower case so if user inputs with a capitol letter like Pikachu, using for validation purposes
