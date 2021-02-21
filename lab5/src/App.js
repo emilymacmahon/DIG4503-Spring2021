2cf49632d28888862f0909cd589b9eb6450a9fe1
emilymacmahon <70771497+emilymacmahon@users.noreply.github.com>
2021-02-20 19:02:47 -0500

    Update App.js
    adding more comments and fixing errors

--- lab5/src/App.js
+++ lab5/src/App.js
@@ -1,72 +1,91 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import "./App.css";
-import axios from 'axios';
+import axios from "axios";
 
 const App = () => {
-  //so I can see in the console the api is working
+  //so I can see if the api is working as i go
   const [pokemon, setPokemon] = useState("pikachu");
   //empty array
   const [pokemonData, setPokemonData] = useState([]);
   //empty string
   const [pokemonType, setPokemonType] = useState("");
 
+  //to lower case used for validation so the search won't be case sensitive
+  //prevent default so the page doesn't reload
   const handleChange = (e) => {
     setPokemon(e.target.value.toLowerCase());
   };
-
-  const handleSubmit= (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     getPokemon();
   };
-
   const getPokemon = async () => {
     const toArray = [];
     try {
-      //fetching the url
-      //${pokemon} gathers an individual pokemon that is searched for
-      const url =`https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+      //fetching url
+      //${pokemon} gtahers the individual poke that is entered
+      const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
       const res = await axios.get(url);
       toArray.push(res.data);
       setPokemonType(res.data.types[0].type.name);
       //setting the actual pokemon data
-      setPokemonData(toArray); //catches any errors and logs them in console
-        }  catch (e) {
-      console.log(e)
+      setPokemonData(toArray);
+    } catch (e) { //catches the errors an logs then to the console
+      console.log(e);
     }
   };
   console.log(pokemonData);
 
- //prevent default so it doesn't reload the page
- //to lower case so if user inputs with a capitol letter like Pikachu, using for validation purposes
-const handleChange = (e) =>{
-  setPokemon(e.target.value.toLoweCase());
-};
-
-const handleSubmit = (e) => {
-  e.preventDefault();
-  getPokemon();
-};
-
-
-  return(
-    //text area rather than a button to enter information
+  return (
+    //text area rather than a button to accept information, submits upon user hitting enter
     <div className="App">
+      <h1>Type Pokemon and hit enter key</h1>
       <form onSubmit={handleSubmit}>
         <label>
-          
-          <input type="text" 
-          onChange={handleChange} 
-          placeholder="Enter Pokemon"/> 
+          <input
+            type="text"
+            onChange={handleChange}
+            placeholder="enter pokemon name"
+          />
         </label>
       </form>
+      {/* <ul>{pokemonData}</ul> */}
+      {/* <p>{[pokemonData]}</p> */}
       {pokemonData.map((data) => {
-        return(
-
-        )
+        return ( //math.round will be used bc poke api used a weird unit of measure, so there is a conversion used
+          <div className="container">
+            <img src={data.sprites["front_default"]} />
+            <div className="divTable">
+              <div className="divTableBody">
+                <div className="divTableRow">
+                  <div className="divTableCell">Type</div>
+                  <div className="divTableCell">{pokemonType}</div>
+                </div>
+                <div className="divTableRow">
+                  <div className="divTableCell">Height</div>
+                  <div className="divTableCell">
+                    {" "}
+                    {Math.round(data.height * 3.9)}"
+                  </div>
+                </div>
+                <div className="divTableRow">
+                  <div className="divTableCell">Weight</div>
+                  <div className="divTableCell">
+                    {" "}
+                    {Math.round(data.weight / 4.3)} lbs
+                  </div>
+                </div>
+                <div className="divTableRow">
+                  <div className="divTableCell">Number of Battles</div>
+                  <div className="divTableCell">{data.game_indices.length}</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        );
       })}
-
     </div>
-  );
+  ); 
 };
 
 export default App;
