e8310acc3c47974d9b16e0ff180ba986b0529e64
emilymacmahon <70771497+emilymacmahon@users.noreply.github.com>
2021-04-11 10:50:44 -0400

    client code

--- lab11/index.js
+++ lab11/index.js
@@ -7,3 +7,50 @@ const port = 45030;
 //translate request to JSON
 App.use(Express.json());
 
+
+//db
+
+App.put("/books/:isbn", (req, res) => {
+    const isbn = req.params.isbn;
+
+    const title = req.body.title;
+    const author = req.body.author;
+    const description = req.body.description;
+
+    res.json({
+        isbn: isbn,
+        title: title,
+        author: author,
+        description: description
+    });
+});
+
+App.get("/books/:isbn", (req, res) => {
+    const isbn = req.params.isbn;
+    res.json({book: "not found"});
+});
+
+App.patch("/books/:isbn", (req, res) => {
+    const isbn = req.params.isbn;
+
+    const title = req.body.title;
+    const author = req.body.author;
+    const description = req.body.description;
+
+    res.json({
+        isbn: isbn,
+        title: title,
+        author: author,
+        description: description
+
+    });
+});
+
+App.delete("/books/:isbn", (req, res) => {
+    const isbn = req.params.isbn;
+
+    res.json({"deleted": 0});
+
+});
+
+App.listen(port);
