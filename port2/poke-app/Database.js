6a0afab8c02049509d1c6507c7a80e19215d7f06
emilymacmahon <70771497+emilymacmahon@users.noreply.github.com>
2021-05-02 13:12:35 -0400

    more db

--- port2/poke-app/Database.js
+++ port2/poke-app/Database.js
@@ -29,6 +29,42 @@ class Database {
 
     }
 
+    async create(document) {
+        let createdResult =null;
+        if(this.collection !=null){
+            createdResult = await this.collectuon.insertOne(document);
+        }
+        return createdResult;
+    }
+
+    async readOne(query){
+        let foundDocument = await this.collection.findOne(query);
+        if (this.colletion != null){
+            foundDocument = await this.collection.findOne(query);
+        }
+        return foundDocument;
+    }
+    async readMany(query){
+        let foundDocuments=null;
+        if (this.collection !=null){
+            foundDocuments =await this.collection.find(query).toArray();
+        }
+        return foundDocuments;
+    }
+    async update(query, update){
+        let updatedResult =null;
+        if (this.collection !=null){
+            updatedResult = await this.collection.updateOne(query, {$set: update})
+        }
+        return updatedResult;
+    }
+    async delete(query) {
+        let deletedResult = null;
+        if (this.collection != null){
+            deletedResult = await this.collection.deleteOne(query);
+        }
+        return deletedResult;
+    }
    
 }
 
